4f58d846bfe0d5ce345c710d0f2e661d12888686
Felix Krückel <hi@felkru.com>
2023-12-07 02:24:30 +0100

    update to astro v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://www.juliaels.com',
+  integrations: [mdx(), sitemap(), tailwind()],
 });
