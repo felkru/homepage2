cb36384683345c7f0c42ee9b1d08a19f70842dce
Felix Krückel <hi@felkru.com>
2023-12-08 14:46:07 +0100

    added latex to markdown, favicon should now work with google. I still want to  make tags work on linkposts and figure out a intuitive markdown setup for julia, put instructions in the readme.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,21 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import remarkMath from 'remark-math';
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.juliaels.com',
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [
+      remarkMath
+    ],
+    rehypePlugins: [
+      [rehypeKatex, {
+        // Katex plugin options
+      }]
+    ]
+  }
 });
